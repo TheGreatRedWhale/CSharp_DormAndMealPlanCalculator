976056d4d162679bc2c3cdae08c06ff5eea6ac74
Jared Whaley <jjw0008@auburn.edu>
2021-09-09 15:01:02 -0400

    - Added functionality to calculate method and temporarily printed output to a messageBox for testing.

--- DormAndMealPlanCalculator/SelectionsForm.cs
+++ DormAndMealPlanCalculator/SelectionsForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,13 @@ namespace DormAndMealPlanCalculator
     public partial class SelectionsForm : Form
     {
         List<Dormitory> dormitories = new List<Dormitory>();
+        List<MealPlan> mealPlans = new List<MealPlan>();
 
         public SelectionsForm()
         {
             InitializeComponent();
             createDorms();
+            createMealPlans();
         }
 
         public void addDorm(string name, int cost)
@@ -45,11 +48,50 @@ namespace DormAndMealPlanCalculator
             }
         }
 
+        public void addMealPlan(int mealsPerWeek, int cost)
+        {
+            mealPlans.Add(new MealPlan(mealsPerWeek, cost));
+            if (mealsPerWeek == -1)
+            {
+                mealsListView.Items.Add("Unlimited").
+                    SubItems.Add(cost.ToString("C2"));
+            }
+            else
+            {
+                mealsListView.Items.Add(mealsPerWeek.ToString()).
+                    SubItems.Add(cost.ToString("C2"));
+            }
+        }
+
+        public void createMealPlans()
+        {
+            addMealPlan(mealsPerWeek: 7, cost: 600);
+            addMealPlan(mealsPerWeek: 14, cost: 1200);
+            addMealPlan(mealsPerWeek: -1, cost: 1700);
+        }
+
+        public void refreshMealPlans()
+        {
+            mealsListView.Clear();
+            for (int i = 0; i < mealPlans.Count; i++)
+            {
+                if (mealPlans[i].MealsPerWeek == -1) {
+                    mealsListView.Items.Add("Unlimited").
+                        SubItems.Add(mealPlans[i].Cost.ToString("C2"));
+                } else {
+                    mealsListView.Items.Add(mealPlans[i].MealsPerWeek.ToString()).
+                        SubItems.Add(mealPlans[i].Cost.ToString("C2"));
+                }
+            }
+        }
+
         public void calculate()
         {
             try
             {
-
+                int dormCost = Int32.Parse(dormsListView.SelectedItems[0].SubItems[1].Text.ToString(), NumberStyles.Currency);
+                int mealPlanCost = Int32.Parse(mealsListView.SelectedItems[0].SubItems[1].Text.ToString(), NumberStyles.Currency);
+                MessageBox.Show((dormCost + mealPlanCost).ToString("C2"));
             }
             catch (Exception e)
             {
