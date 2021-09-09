03ed760bb7ef869ec8ebb82cec9f139d129339ca
Jared Whaley <jjw0008@auburn.edu>
2021-09-09 12:50:53 -0400

    - Added dormsListView and accompanying headers to SelectionsForm. - Created createDorms and addDorm methods. - Called createDorms immediately after form initialization in order to populate the dormsListView.

--- DormAndMealPlanCalculator/SelectionsForm.cs
+++ DormAndMealPlanCalculator/SelectionsForm.cs
@@ -15,6 +15,21 @@ namespace DormAndMealPlanCalculator
         public SelectionsForm()
         {
             InitializeComponent();
+            createDorms();
+        }
+
+        private void addDorm(string dormName, int dormCost)
+        {
+            dormsListView.Items.Add(dormName).
+                SubItems.Add(dormCost.ToString("C"));
+        }
+
+        private void createDorms()
+        {
+            addDorm(dormName: "Allen Hall", dormCost: 1500);
+            addDorm(dormName: "Pike Hall", dormCost: 1600);
+            addDorm(dormName: "Farthing Hall", dormCost: 1800);
+            addDorm(dormName: "University Suites", dormCost: 2500);
         }
     }
 }
